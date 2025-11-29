7f968b5dfb058d2e8f166683297986e540825196
anrui2032 <anrui2032@gmail.com>
2025-11-30 04:29:11 +0800

    nx667j: Import touchscreen firmware from GEN_ZTE_R_P875N02V1.0.2B34
    Change-Id: I76c656f11502f2593f03631c2ed6faa472e4f328

--- nx667j-vendor.mk
+++ nx667j-vendor.mk
@@ -480,6 +480,7 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/nx667j/proprietary/vendor/etc/wifi/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wlan/WCNSS_qcom_cfg.ini \
     vendor/nubia/nx667j/proprietary/vendor/etc/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     vendor/nubia/nx667j/proprietary/vendor/firmware/CAMERA_ICP.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/CAMERA_ICP.elf \
+    vendor/nubia/nx667j/proprietary/vendor/firmware/GT9897_test_sensor_0.tporder:$(TARGET_COPY_OUT_VENDOR)/firmware/GT9897_test_sensor_0.tporder \
     vendor/nubia/nx667j/proprietary/vendor/firmware/a660_gmu.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/a660_gmu.bin \
     vendor/nubia/nx667j/proprietary/vendor/firmware/a660_sqe.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a660_sqe.fw \
     vendor/nubia/nx667j/proprietary/vendor/firmware/a660_zap.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/a660_zap.b00 \
@@ -508,6 +509,10 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/nx667j/proprietary/vendor/firmware/evass.b19:$(TARGET_COPY_OUT_VENDOR)/firmware/evass.b19 \
     vendor/nubia/nx667j/proprietary/vendor/firmware/evass.mbn:$(TARGET_COPY_OUT_VENDOR)/firmware/evass.mbn \
     vendor/nubia/nx667j/proprietary/vendor/firmware/evass.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/evass.mdt \
+    vendor/nubia/nx667j/proprietary/vendor/firmware/goodix_cfg_group.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_cfg_group.bin \
+    vendor/nubia/nx667j/proprietary/vendor/firmware/goodix_cfg_group_gt9897.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_cfg_group_gt9897.bin \
+    vendor/nubia/nx667j/proprietary/vendor/firmware/goodix_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_firmware.bin \
+    vendor/nubia/nx667j/proprietary/vendor/firmware/goodix_firmware_gt9897.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_firmware_gt9897.bin \
     vendor/nubia/nx667j/proprietary/vendor/firmware/ipa_fws.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/ipa_fws.b00 \
     vendor/nubia/nx667j/proprietary/vendor/firmware/ipa_fws.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/ipa_fws.b01 \
     vendor/nubia/nx667j/proprietary/vendor/firmware/ipa_fws.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/ipa_fws.b02 \
